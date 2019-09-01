9c7d40c11848596096f5fdb91d646003ad5fbd44
Miguel Machado <miiguellmp@gmail.com>
2019-09-01 19:00:03 -0300

    user model

---
+++ src/app/models/User.js
@@ -0,0 +1,36 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        cpf: Sequelize.STRING,
+        admin: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
